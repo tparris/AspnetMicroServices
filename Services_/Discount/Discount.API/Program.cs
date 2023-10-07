3a71a08af0db1e98f63a5e24eabec122f858c8a3
tparris <trevor.o.parris@gmail.com>
2023-10-07 18:41:07 -0400

    discount.api changes

--- Services_/Discount/Discount.API/Program.cs
+++ Services_/Discount/Discount.API/Program.cs
@@ -1,4 +1,6 @@
 
+using Discount.API.Repositories;
+
 namespace Discount.API
 {
     public class Program
@@ -6,9 +8,8 @@ namespace Discount.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
             // Add services to the container.
-
+            builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
